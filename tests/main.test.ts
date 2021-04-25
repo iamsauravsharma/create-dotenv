647d4150afdcb79827918d83876637abf3d48621
Saurav Sharma <appdroiddeveloper@gmail.com>
2021-04-25 12:31:32 +0545

    chore: run prettier
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -4,7 +4,7 @@ test("test if env file exists", async () => {
     const envFilePath = "tests/development.env";
     fs.stat(envFilePath, (err, _stats) => {
         if (err) {
-            console.error(err)
+            console.error(err);
         }
-    })
+    });
 });
